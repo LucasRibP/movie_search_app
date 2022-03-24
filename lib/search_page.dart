de4087aa4ed7b2c7664521fab311d0443da0e76b
Lucas <lrpessamilio@gmail.com>
2022-03-24 09:30:29 -0300

    Adds debounce upon text changes

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_search_app/search_bar.dart';
 import 'package:movie_search_app/search_list.dart';
+import "dart:async";
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -10,11 +11,29 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  String searchTerm = "";
-  void onSearchBarTextChange(String text) {
-    setState(() {
-      searchTerm = text;
+  Timer? _debounceMovieSearch;
+
+  // Keys shouldn't be left visible on public projects, I'm only
+  // leaving this one here because it's a free API and there are no
+  // repercussions
+  final String _omdbKey = "df4ec22f";
+
+  void _onSearchBarTextChange(String text) {
+    if (_debounceMovieSearch?.isActive ?? false) _debounceMovieSearch?.cancel();
+    _debounceMovieSearch = Timer(const Duration(milliseconds: 500), () {
+      requestMovieSearch(text);
     });
+  }
+
+  void requestMovieSearch(String searchQuery) {
+    String query =
+        "http://www.omdbapi.com/?apikey=$_omdbKey&s=$searchQuery&type=movie";
+  }
+
+  @override
+  void dispose() {
+    _debounceMovieSearch?.cancel();
+    super.dispose();
   }
 
   @override
@@ -26,7 +45,7 @@ class _SearchPageState extends State<SearchPage> {
       body: Column(
         children: [
           Container(
-            child: SearchBar(onTextChange: onSearchBarTextChange),
+            child: SearchBar(onTextChange: _onSearchBarTextChange),
             margin: const EdgeInsets.all(12),
           ),
           SearchList()
